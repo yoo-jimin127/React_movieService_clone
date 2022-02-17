d92ca68b72b17db1769335f3f0e6156e833f95d7
yoo-jimin127 <dbwlals9936@naver.com>
2022-02-17 14:17:38 +0900

    add : movie app initial setting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,10 @@
 import {useState, useEffect} from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect (() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then(response => response.json())
-    .then(json => {
-      setCoins(json); // coin state change
-      setLoading(false); // loading state change
-    });
-  }, []);
 
   return (
     <div>
-      <h1>The Coins! {coins.length === 0 ? null : `[${coins.length}]` }</h1>
-      {loading ? <strong>Loading...</strong> : 
-      <ul>
-        {coins.map((coin => 
-        <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
-        ))}
-      </ul>}
+
     </div>
   );
 }

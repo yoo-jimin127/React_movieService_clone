b0817442acf520b7c69a9323886e53e783654bc6
yoo-jimin127 <dbwlals9936@naver.com>
2022-02-17 14:14:37 +0900

    add : coin tracker component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,27 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); // form의 자동 새로고침 기능(vanilla JS)을 막는 작업
-    if (toDo === "") {
-      return; // if todo empty -> 작동하지 않도록
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo(""); // 입력된 경우 input 태그 내부의 값을 지워줌
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect (() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then(json => {
+      setCoins(json); // coin state change
+      setLoading(false); // loading state change
+    });
+  }, []);
 
-  return ( 
-  <div>
-    <h1>My To Dos [{toDos.length}]</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-      onChange={onChange} 
-      value={toDo} 
-      type="text" 
-      placeholder="Write your TO DO ..." />
-      <button>Submit request</button>
-    </form>
-    <hr />
-    <ul>
-      {toDos.map((item, index) => <li key={index}>{item}</li>)}
-    </ul>
-  </div>
+  return (
+    <div>
+      <h1>The Coins! {coins.length === 0 ? null : `[${coins.length}]` }</h1>
+      {loading ? <strong>Loading...</strong> : 
+      <ul>
+        {coins.map((coin => 
+        <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
+        ))}
+      </ul>}
+    </div>
   );
 }
 
